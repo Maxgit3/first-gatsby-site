c68c234898b7231e9b4bfe0bc4739d59fcaafc18
Maxgit3 <52330733+Maxgit3@users.noreply.github.com>
2021-07-04 02:15:52 -0400

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "First Gatbsy Site",
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-netlify-cms", 
+  "gatsby-plugin-gatsby-cloud",
+  "gatsby-plugin-image",
+  "gatsby-plugin-sharp",],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 
@@ -12,6 +13,14 @@ const IndexPage = () => {
       <title>Home Page</title>
       <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/icon.png"
+      />
       </Layout>
     </main>
   )
