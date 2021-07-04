cec699b046a16973cb53d2b495299a15b894dbc1
Maxgit3 <52330733+Maxgit3@users.noreply.github.com>
2021-07-03 22:22:29 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "First Gatbsy Site",
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-gatsby-cloud"],
+};
